3a13ceb9d07b6cc845c2bafee82897adf1bbd8fe
Esmeralda Quintana <quintaen195@potsdam.edu>
2017-02-20 20:24:52 -0500

    update bullet_hell.tscn with better player movement, change bhNOTES.txt to CHANGE.LOG
    Changed player to KinematicBody2D object instead of Sprite, in order
    to handle future collision detection.
    Changed BulletHell.gd to handle player movement in _fixed_process()
    instead of _process(). This makes physics/motion calculation more
    predictable since it is called the same amount of times per second.

--- GuamGuamsWorld/bullet_hell/BulletHell.gd
+++ GuamGuamsWorld/bullet_hell/BulletHell.gd
@@ -1,27 +1,51 @@
-extends TextureFrame # Background Node
+extends TextureFrame
 
 var player_health = 100
-var screen_size # holds screen size
-var player_size # hold pad size
+var screen_size
+var player_size
 var player_speed = 300
+# player velocity doesn't need to be initialized
+#		because it is updated to 0 on inactivity
+var player_velocity = Vector2()
 
+# NOTE: BH BOUNDARIES 500x500 box, 8px border, active area is 485x485
 
-
-# BH BOUNDARIES 500x500 box with 8px border meaning active area is 485x485
-
+# fixed_process happens on timed basis
+func _fixed_process(delta):
+	player_velocity.y += delta
+	if (Input.is_action_pressed("move_left")):
+		player_velocity.x = -player_speed
+	elif (Input.is_action_pressed("move_right")):
+		player_velocity.x = player_speed
+	else:
+		player_velocity.x = 0
+	if (Input.is_action_pressed("move_up")):
+		player_velocity.y = - player_speed
+	elif (Input.is_action_pressed("move_down")):
+		player_velocity.y = player_speed
+	else:
+		player_velocity.y = 0
+	var motion = player_velocity * delta
+	get_node("Player").move(motion)
+	
 # first function called is _ready()
 func _ready():
+	# set_process(true)
+	set_fixed_process(true)
 	screen_size = get_viewport_rect().size
+	var boundarybox = get_node("BH Boundaries").get_pos()
+	print("boundarybox_x is", boundarybox.x)
+	print("boundarybox_y is", boundarybox.y)
 	 # pad_size obtains texture size
-	player_size = get_node("BH Boundaries/BH Player").get_texture().get_size()
+	player_size = get_node("Player/Player Sprite").get_texture().get_size()
 	 # activates idle processing
 	 # when active, _process() called on every frame
-	set_process(true)
+	
 
 # gamestate update
 # delta processes at every frame
 func _process(delta):
-	var player_pos = get_node("BH Boundaries/BH Player").get_pos()
+	#var player_pos = get_node("Player").get_pos()
 	#var left_rect = Rect2( get_node("left").get_pos() - pad_size*0.5, pad_size )
 	#var right_rect = Rect2( get_node("right").get_pos() - pad_size*0.5, pad_size )
 	 # get new ball position
@@ -42,17 +66,18 @@ func _process(delta):
 #		direction = Vector2(-1, 2.0*randf()-1)
 		
 		
-	get_node("BH Boundaries/BH Player").set_pos(player_pos) # update player position
+	#get_node("Player").set_pos(player_pos) # update player position
 	
 	 # move left pad
 	#var left_pos = get_node("BH Boundaries/BH Player").get_pos()
-	if (Input.is_action_pressed("move_up")):
-		player_pos.y += -player_speed * delta
-	if (Input.is_action_pressed("move_down")):
-		player_pos.y += player_speed * delta
-	if (Input.is_action_pressed("move_right")):
-		player_pos.x += player_speed * delta
-	if (Input.is_action_pressed("move_left")):
-		player_pos.x += -player_speed * delta
-	get_node("BH Boundaries/BH Player").set_pos(player_pos)
+	#if (Input.is_action_pressed("move_up")):
+		
+	#	player_pos.y += -player_speed * delta
+	#if (Input.is_action_pressed("move_down")):
+	#	player_pos.y += player_speed * delta
+	#if (Input.is_action_pressed("move_right")):
+	#	player_pos.x += player_speed * delta
+	#if (Input.is_action_pressed("move_left")):
+	#	player_pos.x += -player_speed * delta
+	#get_node("Player").set_pos(player_pos)
 	
