053f449e6947208ef8c1ed4b1904abbcb74bcfaa
Esmeralda Quintana <quintaen195@potsdam.edu>
2017-03-06 08:16:39 -0500

    add Timer object to MonHP.gd to slow debugging output

--- GuamGuamsWorld/MonHp.gd
+++ GuamGuamsWorld/MonHp.gd
@@ -1,27 +1,39 @@
 extends Node2D
 
-export var health = 100
+var health = global.player_health
+var t
 
 func _ready():
-	set_process_input(true)
+	#set_process_input(true)
 	set_process(true)
+	set_fixed_process(true)
+	print("MonHP.gd sees initial health as ", health)
+	t = Timer.new()
+	t.set_wait_time(2)
+	self.add_child(t)
+	t.start()
+	print("INIT time left ", t.get_time_left())
 	
-func _input(event):
-	if event.is_action_pressed("ui_right"):
-		if health < 100:
-			health += 10
-	if event.is_action_pressed("ui_left"):
-		if health > 0:
-			health -= 10
-			
 func _draw():
 	var rectangle = Rect2(0,0,(health/1),25)
 	var color = Color(1,0,0,1)
 	draw_rect(rectangle, color)
-	
+
 func _process(delta):
+	health = global.player_health
 	update()
-	
 	if health <= 0:
-		#add some code 
 		print("Oh boy, you have fainted.")
+	if (t.get_time_left() - .3 <= 0):
+		print("TIME UP")
+		print("MonHP.gd sees health as ", global.player_health)
+		t.set_wait_time(3)
+		t.start()
+	
+#func _input(event):
+#	if event.is_action_pressed("ui_right"):
+#		if health < 100:
+#			health += 10
+#	if event.is_action_pressed("ui_left"):
+#		if health > 0:
+#			health -= 10
