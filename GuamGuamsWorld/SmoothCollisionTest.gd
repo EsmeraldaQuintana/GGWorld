d7c5b9a43bf88abb5bdb04be8d116be280c73316
AlexP <pendelaj198@potsdam.edu>
2017-03-06 22:36:34 -0500

    Added smooth collision, I think

---
+++ GuamGuamsWorld/SmoothCollisionTest.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+# Set the constant value to increase movement by
+const WALK_SPEED=200
+
+var velocity=Vector2()
+
+# function that tracks movement
+func _fixed_process(delta):
+	
+	# A series of if/elif/else statements that checks for
+	# input and then moves character.
+	if (Input.is_action_pressed("ui_left")):
+		velocity.x = -WALK_SPEED
+	elif (Input.is_action_pressed("ui_right")):
+		velocity.x = WALK_SPEED
+	else:
+		velocity.x = 0
+	
+	if (Input.is_action_pressed("ui_down")):
+		velocity.y = WALK_SPEED
+	elif (Input.is_action_pressed("ui_up")):
+		velocity.y = -WALK_SPEED
+	else:
+		velocity.y = 0
+		
+	# defines motion based on velocity per second
+	var motion = velocity * delta
+	move(motion)
+	
+	#should be smoother collision?
+	if (is_colliding()):
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
+		move(motion)
+	
+func _ready():
+	set_fixed_process(true)
--- GuamGuamsWorld/bullet_hell/BulletHell.gd
+++ GuamGuamsWorld/bullet_hell/BulletHell.gd
@@ -4,11 +4,11 @@ extends TextureFrame
 # global.player_health for health
 var screen_size
 var player_size
-var player_speed = 300
+#var player_speed = 300
 # player_velocity doesn't need to be initialized
 #		because it is updated to 0 on inactivity
 #		in _fixed_process()
-var player_velocity = Vector2()
+#var player_velocity = Vector2()
 var colliding
 
 # first function called is _ready()
@@ -39,23 +39,24 @@ func _fixed_process(delta):
 			print("Took 40 lightning dmg!")
 			global.player_health = global.player_health - 40
 			print("Player health is ", global.player_health)
+
 	# COLLIDING HANDLING END
 	# PLAYER MOVEMENT
-	player_velocity.y += delta
-	if (Input.is_action_pressed("move_left")):
-		player_velocity.x = -player_speed
-	elif (Input.is_action_pressed("move_right")):
-		player_velocity.x = player_speed
-	else:
-		player_velocity.x = 0
-	if (Input.is_action_pressed("move_up")):
-		player_velocity.y = - player_speed
-	elif (Input.is_action_pressed("move_down")):
-		player_velocity.y = player_speed
-	else:
-		player_velocity.y = 0
-	var motion = player_velocity * delta
-	get_node("Player").move(motion)
+	#player_velocity.y += delta
+	#if (Input.is_action_pressed("move_left")):
+	#	player_velocity.x = -player_speed
+	#elif (Input.is_action_pressed("move_right")):
+	#	player_velocity.x = player_speed
+	#else:
+	#	player_velocity.x = 0
+	#if (Input.is_action_pressed("move_up")):
+	#	player_velocity.y = - player_speed
+	#elif (Input.is_action_pressed("move_down")):
+	#	player_velocity.y = player_speed
+	#else:
+	#	player_velocity.y = 0
+	#var motion = player_velocity * delta
+	#get_node("Player").move(motion)
 	# PLAYER MOVEMENT END
 
 # gamestate update
