c68e86effe3e9726fa367ffacbf4b03d169af67d
durantjm198 <durantjm198@potsdam.edu>
2017-03-29 21:58:27 -0400

    Bulbasaur finished

--- GuamGuamsWorld/bullet_hell/otherPokemon.gd
+++ GuamGuamsWorld/bullet_hell/otherPokemon.gd
@@ -4,6 +4,6 @@
 extends "res://dinos/dino.gd".Dino
 
 func _ready():
-	var otherPokemon = Party.createPokemonInstance(7, 20, 20)
+	var otherPokemon = Party.createPokemonInstance(1, 20, 20)
 	set_texture(otherPokemon.frontSprite)
 	print(otherPokemon.name, " loaded into scene")
