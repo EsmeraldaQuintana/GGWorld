0adcf8b405aa7e81b02b3903445596246e70ba42
Esmeralda Quintana <quintaen195@potsdam.edu>
2017-04-05 21:48:09 -0400

    got timer working in res://bullet_hell/FireballSpawner.gd and restructured, commented.

--- GuamGuamsWorld/bullet_hell/FireballSpawner.gd
+++ GuamGuamsWorld/bullet_hell/FireballSpawner.gd
@@ -3,46 +3,50 @@
 #
 extends KinematicBody2D
 
-#export (int) var bullet_speed = 8
-
 var directional_force = Vector2(0, 3)
-var bullet_gravity = 0
+var shoot_chance = 0 # initialized to 0
+var timer
+var bullet_delay = 2
+var can_shoot = false
 
-export (PackedScene) var scene # Inspector -> Script Variables -> field holds res://bullet_hell/bullet_scene.tscn
-
-#export (NodePath) var bullet_spawn_path
-#onready var bullet_spawn = get_node(bullet_spawn_path)
-
-var shoots = 0 
-
-var timer = null
-var can_shoot = true
-var bullet_delay = 1
+export (PackedScene) var scene # Inspector -> Script Variables -> field holds res://bullet_hell/fireball.tscn
+# ^ scene points to a fireball 
 
 func _ready():
+	_ready_timer() # delays FireballSpawner from shooting
+
+func _ready_timer():
 	set_process(true)
-	
 	timer = Timer.new()
-	timer.set_one_shot(true)
 	timer.set_wait_time(bullet_delay)
+	timer.set_one_shot(true)
 	timer.connect("timeout", self, "on_timeout_complete")
-	add_child(timer)
+	self.add_child(timer)
+	timer.start()
 
 func on_timeout_complete():
 	can_shoot = true
 
 func _process(delta):
-	shoots = randi()%11+1
-	if (shoots > 2):
-		fire_once()
-		shoots = shoots - 1
-		timer.start()
+	shoot_chance = randi()%11+1
+	if ((shoot_chance > 2) && can_shoot):
+		shoot()
 	
-func fire_once():
-	shoot()
-
 func shoot():
 	var bullet = scene.instance()
 	bullet.set_global_pos(get_node("SpawnPoint").get_global_pos())
 	bullet.shoot(directional_force)
 	add_child(bullet)
+	
+##########################################
+# CODE GRAVEYARD
+##########################################
+#export (int) var bullet_speed = 8
+#var bullet_gravity = 0
+
+#export (NodePath) var bullet_spawn_path
+#onready var bullet_spawn = get_node(bullet_spawn_path)
+
+
+#func fire_once():
+#	shoot()
