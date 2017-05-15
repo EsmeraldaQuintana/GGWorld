a6625284fe1e4028500dc57cb57035da9bbdc6f9
Esmeralda Quintana <quintaen195@potsdam.edu>
2017-05-15 15:13:07 -0400

    I accidentally pushed up my big instead of the fix. Amazing.

--- GuamGuamsWorld/global.gd
+++ GuamGuamsWorld/global.gd
@@ -3,5 +3,5 @@ extends Node
 var death = false
 
 func _ready():
-	print("global.gd is running!")
+	#print("global.gd is running!")
 	pass
--- GuamGuamsWorld/overworld/Overworld.gd
+++ GuamGuamsWorld/overworld/Overworld.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
-var death_happened
+#var death_happened
 
 func _ready():
 	print("overworld.gd: in ready()")
-	print("overworld.gd: death happened: ", global.death)
-	if (global.death):
-		_reload()
+	#print("overworld.gd: death happened: ", global.death)
+	#if (global.death):
+	#	_reload()
 	#print("Overworld.gd sees death = ", death)
 
-func _reload():
-	print("overworld.gd: in _reload()...")
-	get_tree().reload_current_scene()
+#func _reload():
+	#print("overworld.gd: in _reload()...")
+	#get_tree().reload_current_scene()
 
 #func _get_death_val():
 #	return death_happened
